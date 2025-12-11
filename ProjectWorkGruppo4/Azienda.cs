0d5c3b1788bbd9b9cf6dcd0dd6bf757353186c1c
FerioliAndrea <FerioliAndrea@CLOUDJ-FERIOLI>
2025-12-11 11:27:31 +0100

    update imp, azienda

--- ProjectWorkGruppo4/Azienda.cs
+++ ProjectWorkGruppo4/Azienda.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,59 @@ namespace ProjectWorkGruppo4
 {
     internal class Azienda
     {
+        string nomeAzienda;
+
+        List<Impiegato> impiegatoList;
+
+        public Azienda(List<Impiegato> impiegatoList) { 
+            this.nomeAzienda = "Azienda generica";
+            this.impiegatoList = impiegatoList;
+        }
+
+        public void aggiungiImpiegato(Impiegato i) //aggiunge un impiegato alla lista
+        {
+            impiegatoList.Add(i);
+        }
+
+        public List<Impiegato> caricaImpiegati(string filePath) //carica da file csv gli impiegati
+        {
+            //string filePath = "C:\\its\\impiegati.csv";
+
+            List<Impiegato> impiegatiList = new List<Impiegato>();
+            string[] righe = File.ReadAllLines(filePath);
+            foreach (string line in righe)
+            {
+                string[] parti = line.Split(',');
+                int idImpiegato = Int32.Parse(parti[0]);
+                string nome = parti[1];
+                string cognome = parti[2];
+                DateTime dataAssunzione = DateTime.Parse(parti[3]);
+                string email = parti[4];
+                string mansione = parti[5];
+                int numeroAumenti = Int32.Parse(parti[6]);
+                string nominativoManager = parti[7];
+                string nomeDipartimento = parti[8];
+                string indirizzoDipartimento = parti[9];
+
+                Impiegato i = new Impiegato(idImpiegato, nome, cognome, dataAssunzione, email, mansione, numeroAumenti, nominativoManager, nomeDipartimento, indirizzoDipartimento);
+                impiegatiList.Add(i);
+            }
+
+            return impiegatiList;
+        }
+
+        public void scriviImpiegati(string filepath) //scrive su file txt gli impiegati
+        {
+            //string filePath = "C:\\its\\impiegato.txt";
+
+            File.WriteAllText(filepath, ""); //pulisce il file prima di scriverci
+
+            foreach (Impiegato i in impiegatoList)
+            {
+                File.AppendAllText(filepath, i.ToString() + "\n");
+            }
+        }
+
+
     }
 }
--- ProjectWorkGruppo4/Impiegato.cs
+++ ProjectWorkGruppo4/Impiegato.cs
@@ -18,5 +18,115 @@ namespace ProjectWorkGruppo4
         string nominativoManager;
         string nomeDipartimento;
         string indirizzoDipartimento;
+
+        public Impiegato(int idImpiegato, string nome, string cognome, DateTime dataAssunzione, string email, string mansione, int numeroAumenti, string nominativoManager, string nomeDipartimento, string indirizzoDipartimento)
+        {
+            this.idImpiegato = idImpiegato;
+            this.nome = nome;
+            this.cognome = cognome;
+            this.dataAssunzione = dataAssunzione;
+            this.email = email;
+            this.mansione = mansione;
+            this.numeroAumenti = numeroAumenti;
+            this.nominativoManager = nominativoManager;
+            this.nomeDipartimento = nomeDipartimento;
+            this.indirizzoDipartimento = indirizzoDipartimento;
+        }
+
+        public int getIdImpiegato()
+        {
+            return idImpiegato;
+        }
+        public void setIdImpiegato(int idImpiegato)
+        {
+            this.idImpiegato = idImpiegato;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getCognome()
+        {
+            return cognome;
+        }
+        public void setCognome(string cognome)
+        {
+            this.cognome = cognome;
+        }
+        public DateTime getDataAssunzione()
+        {
+            return dataAssunzione;
+        }
+        public void setDataAssunzione(DateTime dataAssunzione)
+        {
+            this.dataAssunzione = dataAssunzione;
+        }
+        public string getEmail()
+        {
+            return email;
+        }
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+        public string getMansione()
+        {
+            return mansione;
+        }
+        public void setMansione(string mansione)
+        {
+            this.mansione = mansione;
+        }
+        public int getNumeroAumenti()
+        {
+            return numeroAumenti;
+        }
+        public void setNumeroAumenti(int numeroAumenti)
+        {
+            this.numeroAumenti = numeroAumenti;
+        }
+        public string getNominativoManager()
+        {
+            return nominativoManager;
+        }
+        public void setNominativoManager(string nominativoManager)
+        {
+            this.nominativoManager = nominativoManager;
+        }
+        public string getNomeDipartimento()
+        {
+            return nomeDipartimento;
+        }
+        public void setNomeDipartimento(string nomeDipartimento)
+        {
+            this.nomeDipartimento = nomeDipartimento;
+        }
+        public string getIndirizzoDipartimento()
+        {
+            return indirizzoDipartimento;
+        }
+        public void setIndirizzoDipartimento(string indirizzoDipartimento)
+        {
+            this.indirizzoDipartimento = indirizzoDipartimento;
+        }
+
+        public override string ToString()
+        {
+            string infoImpiegato = $"ID Impiegato: {idImpiegato}\n" +
+                                  $"Nome: {nome}\n" +
+                                  $"Cognome: {cognome}\n" +
+                                  $"Data di Assunzione: {dataAssunzione.ToShortDateString()}\n" +
+                                  $"Email: {email}\n" +
+                                  $"Mansione: {mansione}\n" +
+                                  $"Numero di Aumenti: {numeroAumenti}\n" +
+                                  $"Nominativo Manager: {nominativoManager}\n" +
+                                  $"Nome Dipartimento: {nomeDipartimento}\n" +
+                                  $"Indirizzo Dipartimento: {indirizzoDipartimento}\n";
+            return infoImpiegato;
+        }
     }
 }
