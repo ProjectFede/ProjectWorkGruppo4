b62858739e8dc63c417fb575fd6e2b2e7ddb0d42
ProjectFede <pastori.federicoit@gmail.com>
2025-12-11 11:50:13 +0100

    Update Program.cs
    test1

--- ProjectWorkGruppo4/Program.cs
+++ ProjectWorkGruppo4/Program.cs
@@ -10,7 +10,7 @@ namespace ProjectWorkGruppo4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ciao");
+            Console.WriteLine("ciao pippo");
         }
     }
 }
